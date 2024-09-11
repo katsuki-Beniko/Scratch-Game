8feca7f885217146a7cd9cb427623a1fcefea36b
Katsuki Beniko <bryantiong2902@gmail.com>
2024-09-11 13:40:31 +0800

    update

---
+++ scratch game/getimage.js
@@ -0,0 +1,20 @@
+// Get image
+export const HiddenImageSrc = './car.jpeg'; 
+export const CoverImageSrc = './red-square.png'; 
+export const CongratsImageSrc= './confetti.png'
+//Timer for reset and auto-erase
+export const eraseTimeoutDuration = 20000;  // 20 seconds timeout for auto-erase
+export const resetTimeoutDuration = 2000;  // 2 seconds timeout for resetting game
+
+//adjust cover image 
+export const CoverSizeWidth = 530;  // Width of the cover image
+export const CoverSizeHeight = 500;  // Height of the cover image
+export const threshold = 0.65;  // Threshold: 65% of the cover erased to trigger full reveal
+export const coverPositionX = 75 // position X of the cover image
+export const coverPositionY = 2 // position Y of the cover image
+
+//adjust confetti or congratulation image
+export const congratulationSizeWidth = 600;  // Width of the congratulation image
+export const congratulationSizeHeight = 500;  // Height of the congratulation image
+export const congratulationPositionX = 10 // position X of the congratulation image
+export const congratulationPositionY = 0 // position Y of the congratulation image
