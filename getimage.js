1244bc435f2b1b9756cce406d8481404e6017f2d
Katsuki Beniko <bryantiong2902@gmail.com>
2024-09-12 12:03:17 +0800

    update

--- getimage.js
+++ getimage.js
@@ -7,12 +7,12 @@ export const eraseTimeoutDuration = 20000;  // 20 seconds timeout for auto-erase
 export const resetTimeoutDuration = 2000;  // 2 seconds timeout for resetting game
 
 //adjust cover image 
-export const CoverSizeWidth = 550;  // Width of the cover image
+export const CoverSizeWidth = 600;  // Width of the cover image
 export const CoverSizeHeight = 500;  // Height of the cover image
 export const threshold = 0.65;  // Threshold: 65% of the cover erased to trigger full reveal
-export const coverPositionX = 75 // position X of the cover image
+/*export const coverPositionX = 75 // position X of the cover image
 export const coverPositionY = 2 // position Y of the cover image
-
+*/
 //adjust confetti or congratulation image
 export const congratulationSizeWidth = 600;  // Width of the congratulation image
 export const congratulationSizeHeight = 500;  // Height of the congratulation image
